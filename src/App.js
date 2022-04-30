e11a0b98fd6deec078a9a9e3f5a5ab2d8c97d06e
purrrplelipton <immanueltoby@gmail.com>
2022-04-30 17:26:32 +0100

    update: working perfectly for the most part, just need to update minutely

--- src/App.js
+++ src/App.js
@@ -1,14 +1,70 @@
 import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
 import Columns from "./components/Columns/Columns";
 
-const App = (props) => {
-  const [] = useState();
+const App = () => {
+  const [countries, setCountries] = useState([
+    {
+      name: {
+        common: "Central African Republic"
+      },
+      continents: ["Africa"],
+      timezones: ["UTC+01:00"],
+      cca3: "CAF",
+      ccn3: "140"
+    },
+    {
+      name: {
+        common: "Peabody, KS"
+      },
+      continents: ["South America"],
+      timezones: ["UTC-05:00"],
+      cca3: "USA",
+      ccn3: "840"
+    },
+    {
+      name: {
+        common: "Thailand"
+      },
+      continents: ["Asia"],
+      timezones: ["UTC+07:00"],
+      cca3: "THA",
+      ccn3: "764"
+    },
+    {
+      name: {
+        common: "Canberra ACT"
+      },
+      continents: ["Autralia"],
+      timezones: ["UTC+08:00"],
+      cca3: "AUS",
+      ccn3: "036"
+    },
+    {
+      name: {
+        common: "Embsay, Skipton, UK"
+      },
+      continents: ["Europe"],
+      timezones: ["UTC"],
+      cca3: "GBR",
+      ccn3: "826"
+    }
+  ]);
+
+  useEffect(() => {
+    axios
+     .get('https://restcountries.com/v3.1/all')
+     .then(response => {
+        setCountries(response.data)}
+     ).catch(err => console.log(err))
+    return () => {
+      
+    }
+  }, []);
 
   return (
-    <div className="App">
-      <Columns />
-    </div>
+    <Columns countries={countries} />    
   );
 };
 
--- src/components/Columns/Column/Column.js
+++ src/components/Columns/Column/Column.js
@@ -1,18 +1,73 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
-import classes from "./Column.css";
-
-const brightBg = () => {
-    return (`hsl(${360*Math.random()}, ${25+70*Math.random()}, ${75+10*Math.random()})`);
-};
+import classes from "./Column.module.css";
 
 const Column = (props) => {
+    const [hhDiff,] = props.timezone.split(/:/)
+    const [dateTime, setDateTime] = useState({
+        time: {
+            hh: "hh",
+            mm: "mm",
+            diem: null
+        },
+        day_date: {
+            day: "DDD",
+            date: "?? MMM YYYY"
+        }
+    })
+
+    useEffect(() => {
+        let d = new Date()
+        let utc = d.getTime() + (d.getTimezoneOffset() * 60000)
+        let offset = Number(hhDiff.replace(/[^\d-]/g, ""))
+        const currentTime = new Date(utc + (3.6e+6 * offset))
+        let hh = currentTime.getHours()
+        let mm = currentTime.getMinutes()
+        const weekDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+        const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+        let diem = hh < 12 ? "AM" : "PM"
+        const DotW = weekDays[currentTime.getDay()]
+        hh = hh > 12 ? String(hh - 12).padStart(2,0) : String(hh).padStart(2,0)
+        mm = String(mm).padStart(2,0)
+        setDateTime(dateTime => ({
+            ...dateTime,
+            time: {
+                ...dateTime.time,
+                hh: hh,
+                mm: mm,
+                diem: diem
+            },
+            day_date: {
+                ...dateTime.day_date,
+                day: DotW,
+                date: `${currentTime.getDate()} ${months[currentTime.getMonth()]} ${currentTime.getFullYear()}`
+            }
+        }))
+    }, [])
+
+    const brightBg = () => {
+        return (
+            "hsl(" + (Math.random() * 360) + ", " +
+            (Math.random() * 70 + 15) + "%, " +
+            (Math.random() * 70 + 25) + "%)"
+        );
+    };
+    
     const style = {backgroundColor: brightBg()};
 
     return (
-        <div className={classes.Column} style={style}>
-            
-        </div>
+        <li className={classes.Column} style={style}>
+            <p className={classes.Region}>{props.continent}|{props.name}</p>
+            <h1 className={classes.Time}>{
+                `${dateTime.time.hh}:${dateTime.time.mm}`
+                }<span className={classes.Diem}>{
+                dateTime.time.diem
+                }</span>
+            </h1>
+            <p className={classes.Date}>{
+            `${dateTime.day_date.day}, ${dateTime.day_date.date}`
+            }</p>
+        </li>
     );
 };
 
