0b62b4f8ac6e6fac004adbac1dc22910417b0383
purrrplelipton <immanueltoby@gmail.com>
2022-04-30 17:52:50 +0100

    update: final result of 5 random timezones

--- src/components/Columns/Column/Column.js
+++ src/components/Columns/Column/Column.js
@@ -4,46 +4,53 @@ import classes from "./Column.module.css";
 
 const Column = (props) => {
     const [hhDiff,] = props.timezone.split(/:/)
+    let d = new Date()
+    let utc = d.getTime() + (d.getTimezoneOffset() * 60000)
+    let offset = Number(hhDiff.replace(/[^\d-]/g, ""))
+    const currentTime = new Date(utc + (3.6e+6 * offset))
+    let hh = currentTime.getHours()
+    let mm = currentTime.getMinutes()
+    const weekDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    let diem = hh < 12 ? "AM" : "PM"
+    const DotW = weekDays[currentTime.getDay()]
+    hh = hh > 12 ? String(hh - 12).padStart(2,0) : String(hh).padStart(2,0)
+    mm = String(mm).padStart(2,0)
     const [dateTime, setDateTime] = useState({
         time: {
-            hh: "hh",
-            mm: "mm",
-            diem: null
+            hh: hh,
+            mm: mm,
+            diem: diem
         },
         day_date: {
-            day: "DDD",
-            date: "?? MMM YYYY"
+            day: DotW,
+            date: `${currentTime.getDate()} ${months[currentTime.getMonth()]} ${currentTime.getFullYear()}`
         }
     })
 
     useEffect(() => {
-        let d = new Date()
-        let utc = d.getTime() + (d.getTimezoneOffset() * 60000)
-        let offset = Number(hhDiff.replace(/[^\d-]/g, ""))
-        const currentTime = new Date(utc + (3.6e+6 * offset))
-        let hh = currentTime.getHours()
-        let mm = currentTime.getMinutes()
-        const weekDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-        const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-        let diem = hh < 12 ? "AM" : "PM"
-        const DotW = weekDays[currentTime.getDay()]
-        hh = hh > 12 ? String(hh - 12).padStart(2,0) : String(hh).padStart(2,0)
-        mm = String(mm).padStart(2,0)
-        setDateTime(dateTime => ({
-            ...dateTime,
-            time: {
-                ...dateTime.time,
-                hh: hh,
-                mm: mm,
-                diem: diem
-            },
-            day_date: {
-                ...dateTime.day_date,
-                day: DotW,
-                date: `${currentTime.getDate()} ${months[currentTime.getMonth()]} ${currentTime.getFullYear()}`
-            }
-        }))
-    }, [])
+        setTimeout(() => {
+                setDateTime(dateTime => ({
+                ...dateTime,
+                time: {
+                    ...dateTime.time,
+                    hh: hh,
+                    mm: mm,
+                    diem: diem
+                },
+                day_date: {
+                    ...dateTime.day_date,
+                    day: DotW,
+                    date: `${currentTime.getDate()} ${months[currentTime.getMonth()]} ${currentTime.getFullYear()}`
+                }
+            }))
+        }, 61000)
+        return () => {
+            console.log(
+                // `${props.name} : ${props.timezone}`
+            )
+        }
+    })
 
     const brightBg = () => {
         return (
